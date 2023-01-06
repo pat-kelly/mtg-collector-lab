0ef16b19c393b133ebedcd8c6a9301a2925c4011
Patrick <kelly.patj@gmail.com>
2023-01-06 09:59:06 -0500

    adding new card route and controller, basic ejs view

--- controllers/cards.js
+++ controllers/cards.js
@@ -4,6 +4,11 @@ function index(req, res){
   res.render('cards/index');
 }
 
+function newCard(req, res){
+  res.render('cards/new');
+}
+
 export{
-  index
+  index,
+  newCard as new
 }
--- routes/cards.js
+++ routes/cards.js
@@ -4,7 +4,10 @@ import * as cardsCtrl from '../controllers/cards.js';
 const router = Router()
 
 /* GET users listing. */
-router.get('/', cardsCtrl.index)
+router.get('/', cardsCtrl.index);
+
+// GET localhost:3000/cards/new
+router.get('/new', cardsCtrl.new);
 
 export {
   router
